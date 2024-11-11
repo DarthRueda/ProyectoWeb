b204fea0e5d4d4304b76f03fc2fd3b10c7155460
DarthRueda <ruedaar04@gmail.com>
2024-11-11 20:17:56 +0100

    modified:   views/css/style.css 	modified:   views/home.php 	new file:   views/img/flecha_blanca.png 	new file:   views/img/flecha_roja.png 	new file:   views/img/local2.png 	new file:   views/img/logocerveza.png 	new file:   views/img/logof1.png 	new file:   views/img/menu_alonso.png 	new file:   views/img/menu_perez.png 	new file:   views/img/menu_sainz.png

--- views/home.php
+++ views/home.php
@@ -6,6 +6,7 @@
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
     <header>
@@ -40,7 +41,7 @@
         <div class="row">
             <div class="col-8">
                 <h1>FAST FORMULA, LA COMIDA MAS RÁPIDA</h1>
-                <button>¡Pide ya!</button>
+                <button class="btn-pedir">¡Pide ya!</button>
             </div>
             <div class="col-4">
                 <div class="row flex-row h-100">
@@ -74,50 +75,102 @@
 
     <!-- Menus -->
     <section id="menusGeneral">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <h2>MENÚS MAS VENDIDOS</h2>
-                    <div id="menus">
-                        <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+        <div class="row">
+            <div class="col-12">
+                <h2>MENÚS MAS VENDIDOS</h2>
+                <div id="menus" class="d-flex justify-content-around flex-wrap">
+                    <div class="card" style="width: 18rem;">
+                        <div class="img-container">
+                            <img src="img/menu_alonso.png" class="card-img-top" alt="Menu Alonso">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Menu Alonso</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-add">Añadir</a>
+                            <img src="img/flecha_roja.png" class="flecha-roja" alt="flecha_roja">
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="img-container">
+                            <img src="img/menu_sainz.png" class="card-img-top" alt="Menu Sainz">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Menu Sainz</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-add">Añadir</a>
+                            <img src="img/flecha_roja.png" class="flecha-roja" alt="flecha_roja">
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="img-container">
+                            <img src="img/menu_perez.png" class="card-img-top" alt="Menu Perez">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Menu Perez</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn-add">Añadir</a>
+                            <img src="img/flecha_roja.png" class="flecha-roja" alt="flecha_roja">
+                        </div>
                     </div>
                 </div>
             </div>
-            <button>Todos los menus</button>
+        </div>
+        <div class="text-center mt-4">
+            <button class="btn-menus">Todos los menus</button>
         </div>
     </section>
 
     <!-- Form -->
-    <section>
-        <div class="container-fluid">
+    <section id="form">
+        <div class="form-container text-center">
             <h2>UNETE A NOSOTROS Y DISFRUTA DE TODAS NUESTRAS VENTAJAS</h2>
-            <form>
+            <form class="container-fluid d-flex align-items-center">
                 <input type="text" placeholder="Nombre">
                 <input type="text" placeholder="Apellido">
                 <input type="email" placeholder="Correo">
-                <button>Enviar</button>
+                <button class="btn-enviar">
+                    <img src="img/flecha_blanca.png" alt="Enviar" class="img-enviar">
+                </button>
             </form>
         </div>
     </section>
 
     <!-- Footer -->
     <footer>
-        <div class="container-fluid">
-            <div class="col-3">
-                <h3>FAST FORMULA</h3>
-                <p>La comida más rápida</p>
-            </div>
-            <div class="col-9">
-                <img src="img/logo.png" alt="logo"> <!-- Insertar patrocinador 1 -->
-                <img src="img/logo.png" alt="logo"> <!-- Insertar patrocinador 2 -->
+        <div>
+            <div class="row">
+                <div class="col-5 d-flex justify-content-end align-items-center">
+                    <img src="img/logofull.png" alt="logo" class="footer-logo">
+                </div>
+                <div class="col-7">
+                    <img src="img/logof1.png" alt="logof1">
+                    <img src="img/logocerveza.png" alt="logocerveza">
+                </div>
             </div>
         </div>
+        <hr>
+        <div>
+            <div class="row text-center">
+                <div class="col-12">
+                    <p><b>Terminos y condiciones</b></p>
+                    <p><b>Politica de privacidad</b></p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-12">
+                    <p><b>Trabaja con nosotros</b></p>
+                </div>
+            </div>
+            <div>
+                <div class="row text-center">
+                    <div class="col-6">
+                        <p><b>© 2021 Fast Formula</b></p>
+                    </div>
+                    <div class="col-6">
+                        <!--Logos redes sociales-->
+                    </div>
+                </div>
+            </div>
     </footer>
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
